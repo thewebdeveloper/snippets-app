d60e86f477488f1cb228c63a7719c1b1e4d74db3
thewebdeveloper <abdullah.webdeveloper@gmail.com>
2017-08-08 16:38:00 +0000

    add argparse module

--- snippets.py
+++ snippets.py
@@ -1,4 +1,4 @@
-import logging
+import logging, argparse
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -23,3 +23,14 @@ def get(name):
 	"""
 	logging.error("FIXME: Unimplemented - get({!r})".format(name))
 	return ""
+	
+	
+def main():
+	"""The Main Function"""
+	logging.info("Constructing Parser")
+	parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+	arguments = parser.parse_args()
+	
+	
+if __name__ == "__main__":
+	main()
